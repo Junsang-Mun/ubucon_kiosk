d71d974ba487c685b99cb6672482b9a203f7adfe
Junsang Mun <mun.js@aol.com>
2024-04-16 04:10:27 +0900

    [feat] update status by clicking buttons

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,35 @@
-// place files you want to import through the `$lib` alias in this folder.
+export async function postDataToDatabase(slug) {
+	const dataKey = import.meta.env.VITE_API_KEY;
+	const projectId = dataKey.split('_')[0];
+	const baseName = 'UCK24-Attendee';
+	const headers = {
+		'X-API-Key': dataKey,
+		'Content-Type': 'application/json',
+	};
+	const url = `https://database.deta.sh/v1/${projectId}/${baseName}/items/${slug}`;
+	const data = {
+		set: {
+			checkedIn: true,
+		}
+	}
+	// Make a PATCH request to the database
+	const postResponse = await fetch(url, {
+		method: 'PATCH',
+		headers,
+		body: JSON.stringify(data)
+	});
+
+	if (!postResponse.ok) {
+		// Handle error if the PATCH request fails
+		console.error('Failed to post data to the database');
+		return {
+			status: postResponse.status,
+			body: {
+				error: 'Failed to post data to the database'
+			}
+		};
+	}
+
+	// If the PATCH request is successful, return the response
+	return await postResponse.json();
+}
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,10 +1,15 @@
 <script>
   export let data;
+  import { postDataToDatabase } from "$lib/index.js";
 </script>
 
 <div class="container p-5 m-5">
-  <h1>{data.key}</h1>
-  <h1>{data.attended}</h1>
-  <h1>{data.name}</h1>
-  <h1>{data.org}</h1>
+  <h1>안녕하세요 {data.org}의 {data.name}님!</h1>
+  {#if data.checkedIn}
+    <h1>이미 체크인 되셨습니다.</h1>
+  {:else}
+    <button on:click={() => postDataToDatabase(data.key)}>체크인</button>
+    <h1>체크인 되셨습니다.</h1>
+  {/if}
+  <p>{data.key}</p>
 </div>
