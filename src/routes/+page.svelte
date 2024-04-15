08a7b4d97439acec6f4b04fd21d4e1314a4b3b80
Junsang Moon <mun.js@aol.com>
2024-04-15 23:14:50 +0900

    [feat] remove readers but code_39

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,18 +46,7 @@
     Quagga.decodeSingle(
       {
         decoder: {
-          readers: [
-            "ean_reader",
-            "ean_8_reader",
-            "code_39_reader",
-            "code_39_vin_reader",
-            "codabar_reader",
-            "upc_reader",
-            "upc_e_reader",
-            "i2of5_reader",
-            "2of5_reader",
-            "code_93_reader",
-          ],
+          readers: ["code_39_reader"],
         },
         locate: true,
         src: imageData,
