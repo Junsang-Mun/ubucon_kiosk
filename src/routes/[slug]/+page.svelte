0c5466b2f7c3a237d7e539c750424cf8ab461396
Junsang Mun <mun.js@aol.com>
2024-04-16 13:57:27 +0900

    [feat] detect and print flow completed

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -4,13 +4,23 @@
   import { onMount } from "svelte";
 
   const delay = 3000;
+  let currentUrl;
 
   onMount(() => {
-    setTimeout(() => {
-      window.location.href = "http://localhost:5173";
-    }, delay);
+    if (data.name === undefined || data.org === undefined) {
+      window.location.href = "localhost:5173";
+    }
     postDataToDatabase(data.key);
+    currentUrl = window.location.href;
   });
+  const sendPrintData = () => {
+    const dataToSend = {
+      name: data.name,
+      org: data.org,
+    };
+    const queryParams = new URLSearchParams(dataToSend).toString();
+    window.location.href = `${currentUrl}/print?${queryParams}`;
+  };
 </script>
 
 <div class="container p-5 m-5">
@@ -19,8 +29,20 @@
     <h1>이미 체크인 되셨습니다.</h1>
   {:else if data.checkedIn === false}
     <h1>체크인 되셨습니다.</h1>
+    <button
+      on:click={() => {
+        sendPrintData();
+      }}>인쇄하기</button
+    >
   {:else}
     <h1>체크인 상태를 확인할 수 없습니다.</h1>
   {/if}
   <p>{data.key}</p>
 </div>
+
+<style>
+  @page {
+    size: A6;
+    margin: 2cm;
+  }
+</style>
---
+++ src/routes/[slug]/print/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  let urlParams;
+  let name;
+  let org;
+  const delay = 500;
+
+  onMount(() => {
+    setTimeout(() => {
+      window.print();
+      window.location.href = "http://localhost:5173";
+    }, delay);
+    urlParams = new URLSearchParams(window.location.search);
+    name = urlParams.get("name");
+    org = urlParams.get("org");
+  });
+</script>
+
+<h1>{name}</h1>
+<h1>{org}</h1>
