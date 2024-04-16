dec4d56e455795c3a2212bc8cba7e43431dfcf78
Junsang Mun <mun.js@aol.com>
2024-04-16 17:11:42 +0900

    [feat] update bunch of things

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,16 +3,17 @@
   import { postDataToDatabase } from "$lib/index.js";
   import { onMount } from "svelte";
 
-  const delay = 3000;
   let currentUrl;
+  const timeOut = 2000;
 
   onMount(() => {
-    if (data.name === undefined || data.org === undefined) {
-      window.location.href = "localhost:5173";
-    }
+    if (!data.name) setTimeout(goToHome, timeOut);
     postDataToDatabase(data.key);
     currentUrl = window.location.href;
+    if (data.checkedIn === false) setTimeout(sendPrintData, timeOut);
+    else if (data.checkedIn === true) setTimeout(goToHome, timeOut);
   });
+
   const sendPrintData = () => {
     const dataToSend = {
       name: data.name,
@@ -21,21 +22,33 @@
     const queryParams = new URLSearchParams(dataToSend).toString();
     window.location.href = `${currentUrl}/print?${queryParams}`;
   };
+
+  const goToHome = () => {
+    window.location.href = "http://localhost:5173";
+  };
 </script>
 
 <div class="container p-5 m-5">
-  <h1>안녕하세요 {data.org}의 {data.name}님!</h1>
+  {#if data.org !== undefined || data.name !== undefined}
+    <h1>안녕하세요 {data.org}의 {data.name}님!</h1>
+  {/if}
   {#if data.checkedIn === true}
     <h1>이미 체크인 되셨습니다.</h1>
   {:else if data.checkedIn === false}
     <h1>체크인 되셨습니다.</h1>
-    <button
-      on:click={() => {
-        sendPrintData();
-      }}>인쇄하기</button
-    >
   {:else}
-    <h1>체크인 상태를 확인할 수 없습니다.</h1>
+    <div class="modal-backdrop show" />
+    <div class="modal" tabindex="-1" role="dialog" style="display:block;">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-body">
+            <p>
+              No such key <strong>{data.key}</strong> found in the database.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   {/if}
   <p>{data.key}</p>
 </div>
